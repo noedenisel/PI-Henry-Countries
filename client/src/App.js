ab4ff381a21fab6eabf14bbba7e633bab94b4e47
Noe Denise <noelialombardo@hotmail.com>
2023-02-15 17:35:14 -0300

    Actualice dependencias. Styles index

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
+    <div className="App" >
+      <h1>PI Henry Countries</h1>
+      <h3>By Noelia Lombardo</h3>
+      <button className='button'>Ingresar</button>  {/* //TODO: Linkear el botton a la pagina principal 
+                                  //TODO darle estilos al btn*/}
     </div>
   );
 }
