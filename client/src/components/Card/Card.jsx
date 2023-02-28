d4599e4669352478c40c82147e43f375c23cbbc0
Noe Denise <noelialombardo@hotmail.com>
2023-02-28 16:11:04 -0300

    Card renderiza button onClick ok. Finish

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,26 +1,31 @@
-import { Link } from "react-router-dom"
-import styles from "./Card.module.css"
+import { Link, useLocation } from "react-router-dom";
+import styles from "./Card.module.css";
 
 const Card = (props) => {
+  const location = useLocation();
 
+  // Si la ruta actual es diferente de "/home", no se muestra el botón de cerrar
+  const showCloseButton = location.pathname === "/home";
 
-    return (
-        <div className={styles.card}>
-            <button onClick={props.onClose} className={styles.buttonX}>X</button> 
-            <div className={styles.icon}>
-                <img src= {props.img} alt="" className={styles.img}></img>
-            </div>
-                <p>{props.name}</p>
-            <div className={styles.content}>
-                <p>Continent: {props.continent}</p>
-                 <button className={styles.moreInfo}>
-                 <Link to= {`/home/${props.id}`}>
-                    Mas info
-                 </Link>
-                 </button> {/* //TODO: ponerle el link y estilos */}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.card}>
+      {showCloseButton && (
+        <button onClick={props.onClose} className={styles.buttonX}>
+          X
+        </button>
+      )}
+      <div className={styles.icon}>
+        <img src={props.img} alt="" className={styles.img} />
+      </div>
+      <p>{props.name}</p>
+      <div className={styles.content}>
+        <p>Continent: {props.continent}</p>
+        <button className={styles.moreInfo}>
+          <Link to={`/home/${props.id}`}>Mas info</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
