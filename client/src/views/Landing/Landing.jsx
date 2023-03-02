d78a85585e2b0189fcfc769011ad945ad069887b
Noe Denise <noelialombardo@hotmail.com>
2023-03-02 10:09:30 -0300

    Landing styles finish

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom"
 const Landing = () => {
     return (
         <div className={styles.landingContainer} >
+            <div className={styles.mapa}>
+                <img src="../../img/mapa2.png" alt=""></img>
+            </div>
+            
             <h1 className={styles.titleLanding}>PI Henry Countries</h1>
             
             
